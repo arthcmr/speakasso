979021f071bdc56a41c663c4253b23e2be389bf3
Arthur Camara <arthurcamara@gmail.com>
2014-10-01 19:47:06 +0200

    Improve instructions for second step

--- app/scripts/controllers/instructions.js
+++ app/scripts/controllers/instructions.js
@@ -8,27 +8,42 @@
  * Controller of the perceptionClientApp
  */
 angular.module('perceptionClientApp')
-  .controller('InstructionsCtrl', function ($scope, $rootScope) {
+    .controller('InstructionsCtrl', function($scope, $rootScope) {
 
-  	$rootScope.settings = {
-  		name: "Arthur",
-  		email: "arthurcamara@gmail.com",
-  		language: "english"
-  	}
+        $rootScope.settings = {
+            name: "Arthur",
+            email: "arthurcamara@gmail.com",
+            language: "english"
+        }
 
-  	if(!$rootScope.settings) {
-  		location.href = "#/";
-  		return;
-  	}
 
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+        if (!$rootScope.settings) {
+            location.href = "#/";
+            return;
+        }
 
-    console.log($scope);
-    console.log($rootScope);
+        function hideRoutine() {
+            setTimeout(function() {
+                $(".words").hide();
+                setTimeout(function() {
+                    $(".words").show();
+                }, 1500);
+            }, 2500);
+        }
 
-    $rootScope.prev = "instructions";
-  });
+        $(function() {
+            hideRoutine();
+            setInterval(function() {
+                hideRoutine();
+            }, 4000);
+        });
+
+        $rootScope.prev = "instructions";
+
+        //slide control
+        $scope.slide = 1;
+        $scope.nextSlide = function() {
+            $scope.slide = $scope.slide + 1;
+            console.log($scope.slide);
+        };
+    });
