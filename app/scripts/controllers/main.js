2cdb3ef73bd5700540cbea1c93579d8bafc4d732
Arthur Camara <arthurcamara@gmail.com>
2014-10-06 01:53:09 +0200

    Add language to the beginning

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,8 +10,34 @@
 angular.module('perceptionClientApp')
     .controller('MainCtrl', function($scope, $rootScope) {
 
-        $scope.goFullscreen = function() {
-            $(document).fullScreen(true);
+        $scope.languages = [{
+            value: "english",
+            name: "English",
+            image: "United-States-of-America.png"
+        }, {
+            value: "swedish",
+            name: "Swedish",
+            image: "Sweden.png"
+        }, {
+            value: "chinese",
+            name: "Chinese",
+            image: "China.png"
+        }, {
+            value: "german",
+            name: "German",
+            image: "Germany.png"
+        }, {
+            value: "portuguese",
+            name: "Portuguese",
+            image: "Brazil.png"
+        }, ];
+
+        $scope.selectLanguage = function(lang) {
+            $rootScope.settings = {
+                language: lang
+            }
+            location.href = "#/intro";
+            return;
         }
 
         $rootScope.prev = "main";
