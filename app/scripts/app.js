cd2c455e51719579e4c0ea0021f77370cf39d198
Arthur Camara <arthurcamara@gmail.com>
2015-03-15 00:02:44 +0100

    Attempt to solve rendering problem

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -11,30 +11,35 @@ var API_BASE = 'http://178.62.207.54:3000/';
  * Main module of the application.
  */
 angular
-  .module('speakassoClientApp', [
-    'ngAnimate',
-    'ngCookies',
-    'ngResource',
-    'ngRoute',
-    'ngSanitize',
-    'ngTouch',
-    'ngDropdowns'
-  ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .when('/config', {
-        templateUrl: 'views/config.html',
-        controller: 'ConfigCtrl'
-      })
-      // .when('/canvas', {
-      //   templateUrl: 'views/canvas.html',
-      //   controller: 'CanvasCtrl'
-      // })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+    .module('speakassoClientApp ', [
+        'ngAnimate',
+        'ngCookies',
+        'ngResource',
+        'ngRoute',
+        'ngSanitize',
+        'ngTouch',
+        'ngDropdowns'
+    ])
+    .config(function($routeProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: 'views/main.html',
+                controller: 'MainCtrl'
+            })
+            .when('/config', {
+                templateUrl: 'views/config.html',
+                controller: 'ConfigCtrl'
+            })
+            .when('/canvas', {
+                templateUrl: 'views/canvas.html',
+                controller: 'CanvasCtrl'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+    });
+
+
+window.onload = function() {
+    angular.bootstrap(document.body, ['speakassoClientApp']);
+}
